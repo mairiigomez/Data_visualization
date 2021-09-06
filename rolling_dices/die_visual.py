9d3c539d29821c62620c70061b7db09c3e8808d6
Mairi <mairiigomez@gmail.com>
2021-09-06 18:26:39 -0500

    rolling two dies and changing a for to list comprehension

--- rolling_dices/die_visual.py
+++ rolling_dices/die_visual.py
@@ -3,25 +3,24 @@ from plotly import offline
 from die import Die
 
 # Create a D6
-die = Die()
+die_1 = Die()
+die_2 = Die()
 
-results = []
-# Make some rolls and store the result in a list
-for roll_num in range(1000):
-    result = die.roll_die()
-    results.append(result)
+# Changing in it for list comprenhension
+results = [die_1.roll_die() + die_2.roll_die() for _ in range(1000)]
 
 # Analize the results
 frequencies = []
-for value in range(1,die.number_sides+1):
+max_result = die_1.number_sides + die_2.number_sides
+for value in range(2,max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Visualize the results
-x_values = list(range(1, die.number_sides+1))
+x_values = list(range(2, max_result+1))
 data = [Bar(x=x_values, y=frequencies)]
 
-x_axis_config = {'title': 'Result'}
+x_axis_config = {'title': 'Result', 'dtick':1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Results of Rolling one D6 1000 times', xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout':my_layout}, filename='d6.html')
+my_layout = Layout(title='Results of Rolling two D6 1000 times', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout':my_layout}, filename='d6_d6.html')
