5f08179b8d3fc9358436c15b63f5812a66d5aeec
Mairi <mairiigomez@gmail.com>
2021-09-06 14:51:46 -0500

    used color map, savefig, scatter

--- auto_data_scatter_squares.py
+++ auto_data_scatter_squares.py
@@ -6,7 +6,12 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+
+# Sending a tuple as a color RGB colors closer to 0 dark to 1 lighter
+"ax.scatter(x_values, y_values, c=(0, 0.8, 0), s=10)"
+
+# Using color maps assing a color to lower values and a different to higher
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Set chart tittle and label axes
 ax.set_title("Square number", fontsize=24)
@@ -16,4 +21,8 @@ ax.set_ylabel("Square of value", fontsize=14)
 # Set the range for each axis minimun a maximum values for x and y 
 ax.axis([0, 1100, 0, 1100000])
 
-plt.show()
+# this command show the figure right away
+"plt.show()"
+
+# This comand save the plot to a file
+plt.savefig('squares_plot.png', bbox_inches='tight')
